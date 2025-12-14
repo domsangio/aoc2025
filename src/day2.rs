a304a63c9d6fa2848ff0596cd37e15a09b0258a9
Domenic SanGiovanni <domsangio@gmail.com>
2025-12-13 20:44:17 -0500

    ALmost done with day2b, have a collision issue

--- src/day2.rs
+++ src/day2.rs
@@ -1,16 +1,3 @@
-pub fn part1(input: &str) -> i64 {
-    let mut count: i64 = 0;
-    for parts in input.lines().next().unwrap().split(",") {
-        let (start_str, end_str) = parts.split_once("-").unwrap();
-        let spans = get_spans(start_str, end_str);
-
-        for (start_str, end_str, start_num, end_num) in spans {
-            count += get_count_in_range_equal_digits(&start_str, &end_str);
-        }
-    }
-    count
-}
-
 fn get_spans(start_str: &str, end_str: &str) -> Vec<(String, String, i64, i64)> {
     let mut span: Vec<(String, String, i64, i64)> = Vec::new();
     let start_num = start_str.parse::<i64>().unwrap();
@@ -19,21 +6,15 @@ fn get_spans(start_str: &str, end_str: &str) -> Vec<(String, String, i64, i64)> 
     let mut curr_digits = start_str.len();
     let mut curr_num = start_num;
     while curr_digits <= end_str.len() {
-        if curr_digits % 2 != 0 {
-            // 10 ^ digits - 1 keep in mind
-            curr_num = 10_i64.pow(curr_digits as u32);
-            curr_digits += 1;
-        } else {
-            let curr_end_num = end_num.min(10_i64.pow(curr_digits as u32) - 1);
-            span.push((
-                curr_num.to_string(),
-                curr_end_num.to_string(),
-                curr_num,
-                curr_end_num,
-            ));
-            curr_digits += 2;
-            curr_num = 10_i64.pow(curr_digits as u32);
-        }
+        let curr_end_num = end_num.min(10_i64.pow(curr_digits as u32) - 1);
+        span.push((
+            curr_num.to_string(),
+            curr_end_num.to_string(),
+            curr_num,
+            curr_end_num,
+        ));
+        curr_digits += 1;
+        curr_num = 10_i64.pow(curr_digits as u32 - 1);
     }
 
     return span;
@@ -93,133 +74,130 @@ fn get_last_num_in_range(end_str: &str, end: i64) -> i64 {
     }
 }
 
-// say we start with a number say
+pub fn part1(input: &str) -> i64 {
+    let mut count: i64 = 0;
+    for parts in input.lines().next().unwrap().split(",") {
+        let (start_str, end_str) = parts.split_once("-").unwrap();
+        let spans = get_spans(start_str, end_str);
 
-// start 1 - 42450123
-
-// let num = 42450123;
-// let num_digits = 8;
-// let half_num_digits = num_digits / 2;
-
-// let ten_exponent = 10_i64.pow(half_num_digits as u32);
-// let first_half = num / ten_exponent;
-// let possible_first_num = first_half * ten_exponent + first_half;
-// if num < possible_first_num {
-//     num = first_half + 1 * ten_exponent;
-// }
-
-// start 2 - 42457234
-
-pub fn part2(input: &str) -> i32 {
-    0
+        for (start_str, end_str, start_num, end_num) in spans {
+            count += get_count_in_range_equal_digits(&start_str, &end_str);
+        }
+    }
+    count
 }
 
-// pub fn part1(input: &str) -> i64 {
-//     let mut count: i64 = 0;
-//     for parts in input.lines().next().unwrap().split(",") {
-//         let (start_str, end_str) = parts.split_once("-").unwrap();
-//         let spans = get_spans(start_str, end_str);
+fn get_repeated_num_from_base_and_digits(
+    base_num: i64,
+    base: usize,
+    num_digits_in_base: usize,
+) -> i64 {
+    (0..num_digits_in_base)
+        .map(|i| base_num * (10_i64.pow(base as u32)).pow(i as u32))
+        .sum()
+}
 
-//         for (start_str, end_str, start_num, end_num) in spans {
-//             count += get_count_in_range_equal_digits(&start_str, &end_str);
-//         }
-//     }
-//     count
-// }
+// given a number like 784323401 return 784784784 (the first repeated number)
+fn get_first_repeated_num(start_str: &str, start_num: i64, base: usize) -> i64 {
+    let num_digits = start_str.len();
+    if num_digits % base != 0 {
+        panic!("Number of digits must be divisible by base");
+    }
 
-// fn get_spans(start_str: &str, end_str: &str) -> Vec<(String, String, i64, i64)> {
-//     let mut span: Vec<(String, String, i64, i64)> = Vec::new();
-//     let start_num = start_str.parse::<i64>().unwrap();
-//     let end_num = end_str.parse::<i64>().unwrap();
+    let num_digits_in_base = num_digits / base;
+    let base_num = start_str[0..base].parse::<i64>().unwrap();
+    let repeated_num = get_repeated_num_from_base_and_digits(base_num, base, num_digits_in_base);
+    // do the bounds checking here to make it simpler elsewhere
+    if repeated_num >= start_num {
+        return base_num;
+    } else {
+        return base_num + 1;
+    }
+}
 
-//     let mut curr_digits = start_str.len();
-//     let mut curr_num = start_num;
-//     while curr_digits <= end_str.len() {
-//         if curr_digits % 2 != 0 {
-//             // 10 ^ digits - 1 keep in mind
-//             curr_num = 10_i64.pow(curr_digits as u32);
-//             curr_digits += 1;
-//         } else {
-//             let curr_end_num = end_num.min(10_i64.pow(curr_digits as u32) - 1);
-//             span.push((curr_num.to_string(), curr_end_num.to_string(), curr_num, curr_end_num));
-//             curr_digits += 2;
-//             curr_num = 10_i64.pow(curr_digits as u32);
-//         }
-//     }
+fn get_last_repeated_num(end_str: &str, end_num: i64, base: usize) -> i64 {
+    let num_digits = end_str.len();
+    if num_digits % base != 0 {
+        panic!("Number of digits must be divisible by base");
+    }
 
-//     return span;
-// }
+    let num_digits_in_base = num_digits / base;
+    let base_num = end_str[0..base].parse::<i64>().unwrap();
+    let repeated_num = get_repeated_num_from_base_and_digits(base_num, base, num_digits_in_base);
 
-// fn get_count_in_range_equal_digits(start_str: &str, end_str: &str) -> i64 {
-//     if start_str.len() != end_str.len() {
-//         panic!("Start and end must have the same number of digits");
-//     }
+    if repeated_num <= end_num {
+        return base_num;
+    } else {
+        return base_num - 1;
+    }
+}
 
-//     let start = start_str.parse::<i64>().unwrap();
-//     let end = end_str.parse::<i64>().unwrap();
+fn get_sum_invalid_ids_for_range(
+    start_base: i64,
+    end_base: i64,
+    num_digits: usize,
+    base: usize,
+) -> i64 {
+    if start_base > end_base {
+        return 0;
+    }
 
-//     let start_half = get_first_num_in_range(start_str, start);
-//     let end_half = get_last_num_in_range(end_str, end);
+    println!("Invalid start base: {}, end base: {}", start_base, end_base);
 
-//     if start_half > end_half {
-//         return 0;
-//     } else if start_half == end_half {
-//         return start_half * 10_i64.pow(start_str.len() as u32 / 2) + start_half;
-//     } else {
-//         let partial_sum = (end_half - start_half + 1) * (start_half + end_half) / 2;
-//         return partial_sum * 10_i64.pow(start_str.len() as u32 / 2) + partial_sum;
-//     }
-// }
+    // careful of order here due to truncation of integer division
+    let sum_of_nums = (end_base + start_base) * (end_base - start_base + 1) / 2;
 
-// // technically we only need the half when we do this
-// fn get_first_num_in_range(start_str: &str, start: i64) -> i64 {
-//     /*
-//      * Basically map a number here say d1d2d3d4d5d6d7d8 to a number e1e2e3e40000 such that
-//      * e1e2e3e4e1e2e3e4 >= d1d2d3d4d5d6d7d8 and e4 <= d4+1 ie this is the next number in the sequence
-//      */
-//     let num_digits = start_str.len();
-//     let half_num_digits = num_digits / 2;
-//     let ten_exponent = 10_i64.pow(half_num_digits as u32);
-//     let first_half = start / ten_exponent;
-//     let possible_first_num = first_half * ten_exponent + first_half;
-//     if start <= possible_first_num {
-//         first_half // * ten_exponent + first_half
-//     } else {
-//         (first_half + 1) // * ten_exponent + (first_half + 1)
-//     }
-// }
+    let repeating_num_length = num_digits / base;
+    let pow_sum = (0..base)
+        .map(|i| 10_i64.pow(repeating_num_length as u32).pow(i as u32))
+        .sum::<i64>();
 
-// fn get_last_num_in_range(end_str: &str, end: i64) -> i64 {
-//     let num_digits = end_str.len();
-//     let half_num_digits = num_digits / 2;
-//     let ten_exponent = 10_i64.pow(half_num_digits as u32);
-//     let first_half = end / ten_exponent;
-//     let possible_last_num = first_half * ten_exponent + first_half;
+    println!("Sum of nums: {}, pow sum: {}", sum_of_nums, pow_sum);
+    println!("Result: {}", sum_of_nums * pow_sum);
+    sum_of_nums * pow_sum
+}
 
-//     if end >= possible_last_num {
-//         first_half // * ten_exponent + (first_half - 1)
-//     } else {
-//         first_half - 1 // * ten_exponent + first_half
-//     }
-// }
+// given the number is the same base
+fn get_repeated_count(start_str: &str, end_str: &str, start_num: i64, end_num: i64) -> i64 {
+    // base is the amount of times we repeat the number
+    (1..(start_str.len() / 2 + 1))
+        .map(|base| {
+            // println!("Length of repeating number: {}", base);
+            if start_str.len() % base != 0 {
+                return 0;
+            }
+            let start_base_num = get_first_repeated_num(start_str, start_num, base);
+            let end_base_num = get_last_repeated_num(end_str, end_num, base);
 
-// // say we start with a number say
+            // println!("start repeating number: {}, end repeating number: {}", start_base_num, end_base_num);
+            if start_base_num <= end_base_num {
+                return get_sum_invalid_ids_for_range(
+                    start_base_num,
+                    end_base_num,
+                    start_str.len(),
+                    start_str.len() / base,
+                );
+            }
+            0
+        })
+        .sum()
+}
 
-// // start 1 - 42450123
-
-// // let num = 42450123;
-// // let num_digits = 8;
-// // let half_num_digits = num_digits / 2;
-
-// // let ten_exponent = 10_i64.pow(half_num_digits as u32);
-// // let first_half = num / ten_exponent;
-// // let possible_first_num = first_half * ten_exponent + first_half;
-// // if num < possible_first_num {
-// //     num = first_half + 1 * ten_exponent;
-// // }
-
-// // start 2 - 42457234
-
-// pub fn part2(input: &str) -> i32 {
-//     0
-// }
+pub fn part2(input: &str) -> i64 {
+    input
+        .split(",")
+        .map(|start_and_end| {
+            let (start_str, end_str) = start_and_end.split_once("-").unwrap();
+            // println!("Start str: {}, end str: {}", start_str, end_str);
+            let spans = get_spans(start_str, end_str);
+            // println!("Spans: {:?}", spans);
+            spans
+                .iter()
+                .map(|(start_str, end_str, start_num, end_num)| {
+                    println!("start_str: {}, end_str: {}", start_str, end_str);
+                    get_repeated_count(start_str, end_str, *start_num, *end_num)
+                })
+                .sum::<i64>()
+        })
+        .sum()
+}
