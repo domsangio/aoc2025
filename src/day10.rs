d0e44b1bfdb6f7657670b9b45dbbabe2a47c3a7b
domsangio <domenic_sangiovanni@yahoo.com>
2025-12-24 12:51:23 -0500

    Thoughts and parsing on 10a

--- src/day10.rs
+++ src/day10.rs
@@ -1,44 +1,62 @@
 // we read the input and saw that the longest string is 10 chars, so we can use u16
+
+#[derive(Debug)]
 struct Switch {
-    flip: u16,    // binary number of the flip
-    voltage: u16, // voltage associated with the flip
+    flip: u16, // binary number of the flip
 }
 
+impl Switch {
+    fn print(&self) {
+        print!("[");
+        (0..16).for_each(|i| {
+            if (self.flip >> i) & 1 == 1 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        });
+        println!("]");
+    }
+}
+
+#[derive(Debug)]
 struct ParsedInput {
-    end_goal: u16,
+    end_goal: Switch,
     switches: Vec<Switch>,
+    voltages: Vec<u16>, // voltage associated with the bit being flipped, same size as end_goal
 }
 
 // parses [##...###] into binary 11000111
-fn parse_end_goal(raw_end_goal: &str) -> u16 {
-    raw_end_goal[1..raw_end_goal.len()]
-        .chars()
-        .enumerate()
-        .map(|(i, c)| 2_u16.pow(i as u32) * (if c == '#' { 1 } else { 0 }))
-        .sum()
+fn parse_end_goal(raw_end_goal: &str) -> Switch {
+    Switch {
+        flip: raw_end_goal[1..raw_end_goal.len()]
+            .chars()
+            .enumerate()
+            .map(|(i, c)| 2_u16.pow(i as u32) * (if c == '#' { 1 } else { 0 }))
+            .sum(),
+    }
 }
 
 // parses the (2,3,5,6,8) into a switch of binary, but doesnt set voltage yet
 fn parse_switch_block(switch: &str) -> Switch {
-    let switch_binary: u16 = switch[1..switch.len()]
+    let switch_binary: u16 = switch[1..switch.len() - 1]
         .split(",")
         .map(|num| num.parse::<u32>().unwrap())
         .map(|pow| 2_u16.pow(pow))
         .sum();
     Switch {
         flip: switch_binary as u16,
-        voltage: 0,
     }
 }
 
 fn parse_input_line(line: &str) -> ParsedInput {
     let sections: Vec<&str> = line.split_whitespace().collect();
     let end_goal = parse_end_goal(sections[0]);
-    let mut switches: Vec<Switch> = sections[1..sections.len() - 1]
+    let switches: Vec<Switch> = sections[1..sections.len() - 1]
         .iter()
         .map(|switch: &&str| parse_switch_block(*switch))
         .collect();
-    sections
+    let voltages = sections
         .last()
         .unwrap()
         .strip_prefix('{')
@@ -47,16 +65,40 @@ fn parse_input_line(line: &str) -> ParsedInput {
         .unwrap()
         .split(",")
         .map(|num| num.parse::<u16>().unwrap())
-        .enumerate()
-        .for_each(|(i, voltage)| {
-            switches[i].voltage = voltage;
-        });
+        .collect();
 
-    ParsedInput { end_goal, switches }
+    ParsedInput {
+        end_goal,
+        switches,
+        voltages,
+    }
 }
 
+/**
+ * Thinking out loud, there are only so many combinations. XOR has the communitative property, so
+ * when we flip the same switch twice, it should cancel itself out. If thats true, then in the final
+ * solution, the switch is either flipped once or not flipped ever -- so to find the solution, its a
+ * tree where a switch is included or not; almost another bit mask to do this? or just DP solution
+ * DP(current_state, i) ==> DP(current_state XOR switch i, i+1) + DP(current_state, i+1). This gives us runtime
+ * 2^n where n is the number of switches -- can we do better? BFS could possibly be better with lots of memory
+ * overhead
+ *
+ * But we need to find the fewest switches flipped for part 1, so BFS does sound like a good idea
+ */
+
 pub fn part1(input: &str) -> i64 {
-    input.lines().map(|line| parse_input_line(line)).map(|parsed_input| {});
+    println!("{}", input);
+    input
+        .lines()
+        .map(|line| parse_input_line(line))
+        .for_each(|parsed_input| {
+            print!("End State: ");
+            parsed_input.end_goal.print();
+            parsed_input.switches.iter().enumerate().for_each(|(i, switch)| {
+                print!("Switch {:2}: ", i);
+                switch.print();
+            });
+        });
 
     return 0;
 }
