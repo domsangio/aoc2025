4c0002f1c967054811fea85a205779f875f9f2a5
domsangio <domenic_sangiovanni@yahoo.com>
2025-12-25 09:54:06 -0500

    10b in progress, unit tests failing

--- src/day10.rs
+++ src/day10.rs
@@ -143,11 +143,56 @@ pub fn part1(input: &str) -> i64 {
     input.lines().map(|line| parse_input_line(line)).map(bfs1).sum()
 }
 
+/**
+ * num_bits so we dont have to iterate over the entire switch
+ */
+struct ParsedInput2 {
+    num_bits: u8,
+    switches: Vec<Switch>,
+    voltages: Vec<u16>, // of course the input is larger than u8
+}
+
+// parses the (2,3,5,6,8) into a switch of binary, but doesnt set voltage yet
+fn parse_switch_block2(switch: &str) -> Switch {
+    let switch_binary: u16 = switch[1..switch.len() - 1]
+        .split(",")
+        .map(|num| num.parse::<u32>().unwrap())
+        .map(|pow| 2_u16.pow(pow))
+        .sum();
+    Switch {
+        flip: switch_binary as u16,
+    }
+}
+
+fn parse_input_line2(line: &str) -> ParsedInput2 {
+    let sections: Vec<&str> = line.split_whitespace().collect();
+    let switches: Vec<Switch> = sections[1..sections.len() - 1]
+        .iter()
+        .map(|switch: &&str| parse_switch_block2(*switch))
+        .collect();
+    let voltages: Vec<u16> = sections
+        .last()
+        .unwrap()
+        .strip_prefix('{')
+        .unwrap()
+        .strip_suffix('}')
+        .unwrap()
+        .split(",")
+        .map(|num| num.parse::<u16>().unwrap())
+        .collect();
+
+    ParsedInput2 {
+        num_bits: voltages.len() as u8,
+        switches,
+        voltages,
+    }
+}
 
 /**
- * input: (Current state s of switch increments, count of steps = k)
+ * input: (Current state s of switch increments, count of steps = k, Vec of remaining switches to flip)
+ * queue of future states = (0 state of switch increments, 0 steps, remaining switches indices)
  *
- * queue of future states = (0 state of switch increments, 0 steps)
+ *
  *
  * while queue is not empty;
  *     (curr state, curr steps, curr flipped switches) = queue.pop
@@ -158,38 +203,126 @@ pub fn part1(input: &str) -> i64 {
  *             queue.push new state, steps + 1        
  */
 
- fn apply_switch_to_state(curr_state: Vec<u16>, switch: Switch) -> Vec<u16> {
-    let mut new_state = curr_state.clone();
+struct State {
+    num_bits: u8,
+    num_steps: u16,
+    curr_counts: Vec<u16>,
+    remaining_switches: u16, // bitmask of remaining switches to flip
+}
 
-    (0..16).map(|i| {
-        if 1 << i
-    })
-
-    new_state
- }
-
- fn bfs2(parsed_input: ParsedInput) -> i64 {
-    let mut queue = VecDeque::new();
-    let new_state: Vec<u16> = vec![0; parsed_input.voltages.len()];
-    queue.push_back((new_state, 0));
-
-    while let Some((curr_state, curr_steps)) = queue.pop_front() {
-        for (i, switch) in parsed_input.switches.iter().enumerate() {
-
-            if !curr_flipped_switches[i] {
-                curr_flipped_switches[i] = true;
-                let new_state = curr_state ^ switch.flip;
-                if new_state == parsed_input.end_goal.flip {
-                    return curr_steps + 1;
+impl State {
+    fn initial(target_voltages: &Vec<u16>) -> State {
+        State {
+            num_bits: target_voltages.len() as u8,
+            num_steps: 0,
+            curr_counts: vec![0; target_voltages.len()],
+            remaining_switches: (1 << target_voltages.len()) - 1,
+        }
+    }
+    // returns None if we overflowed the target voltage, otherwise a new state representing the new state
+    // -- is it worth determining remaining switches, and if so when, when constructing a new state, but then we are doing
+    // all the time; since we already determine, when we hit None, drop the switch that produced it (outside this function)
+    // also outside this function, determine if equal to voltage
+    fn apply_switch(&self, switch: &Switch, target_voltage: &Vec<u16>) -> Option<State> {
+        let mut new_counts = self.curr_counts.clone();
+        for i in 0..self.num_bits {
+            if (1 << i) & switch.flip != 0 {
+                new_counts[i as usize] += 1;
+                if new_counts[i as usize] > target_voltage[i as usize] {
+                    return None;
                 }
-                queue.push_back((new_state, curr_steps + 1, curr_flipped_switches.clone()));
             }
         }
+
+        Some(State {
+            num_bits: self.num_bits,
+            num_steps: self.num_steps + 1,
+            curr_counts: new_counts,
+            remaining_switches: self.remaining_switches,
+        })
+    }
+
+    fn is_target_voltage(&self, target_voltage: &Vec<u16>) -> bool {
+        for i in 0..self.num_bits {
+            if self.curr_counts[i as usize] != target_voltage[i as usize] {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+// one thing to note is that we did not maintain a master visited, and instead are doing BFS. if there are many steps, we might be in trouble
+// here
+fn bfs2(parsed_input: ParsedInput2) -> i64 {
+    let mut queue = VecDeque::new();
+    let starting_state = State::initial(&parsed_input.voltages);
+    queue.push_back(starting_state);
+
+    let mut iterations = 0;
+    while let Some(current_state) = queue.pop_front() {
+        iterations += 1;
+        if iterations % 10000 == 0 {
+            println!("Iteration: {}", iterations);
+        }
+
+        let mut new_states: Vec<State> = Vec::new();
+        let mut remaining_switches = current_state.remaining_switches;
+
+        for (i, switch) in parsed_input.switches.iter().enumerate() {
+            if (1 << i) & current_state.remaining_switches != 0 {
+                match current_state.apply_switch(switch, &parsed_input.voltages) {
+                    Some(new_state) => {
+                        if new_state.is_target_voltage(&parsed_input.voltages) {
+                            return new_state.num_steps as i64;
+                        }
+                        new_states.push(new_state);
+                    }
+                    None => {
+                        // we deduced that adding switch i will overflow target, which means this is no longer valid
+                        // for any of the new states since the new state is monotonic, so we remove it from all next states
+                        remaining_switches &= !(1 << i);
+                    }
+                }
+            }
+        }
+
+        for new_state in &mut new_states {
+            new_state.remaining_switches = remaining_switches;
+        }
+        queue.extend(new_states);
     }
 
     panic!("No solution found");
 }
 
 pub fn part2(input: &str) -> i64 {
-    0
+    input.lines().map(|line| parse_input_line2(line)).map(bfs2).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day10::{State, Switch};
+
+    #[test]
+    fn test_apply_switch() {
+        let current_state = State {
+            num_bits: 4,
+            num_steps: 2,
+            curr_counts: vec![1, 2, 3, 4],
+            remaining_switches: 0b1,
+        };
+
+        let target_voltages: Vec<u16> = vec![4, 4, 4, 4];
+
+        let switch = Switch { flip: 0b1010 };
+
+        let new_state_option = current_state.apply_switch(&switch, &target_voltages);
+        assert!(new_state_option.is_some());
+
+        let new_state = new_state_option.unwrap();
+        assert_eq!(new_state.num_steps, 3);
+        assert_eq!(new_state.curr_counts, vec![2, 2, 4, 4]);
+        assert_eq!(new_state.remaining_switches, 0b1);
+    }
 }
